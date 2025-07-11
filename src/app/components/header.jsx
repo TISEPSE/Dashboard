641898f8a20fa14503956dbe45f2a5aa8256813b
TISEPSE <titi21player@gmail.com>
2025-07-11 16:34:00 +0200

    plus styler

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -2,12 +2,12 @@
 
 export default function Header() {
   return (
-    <div className="h-16 bg-gray-900 text-white flex items-center px-4 z-50 shadow-md">
-      <span className="text-xl font-semibold">Navbar styler</span>
+    <div className="h-16 bg-[#212631] text-white flex items-center border-b-1 border-gray-500 px-4 z-50 shadow-md">
+      <span className="text-xl font-semibold">Navbar icon</span>
       <div className="flex items-center w-full max-w-xs ml-4">
         <textarea
           placeholder="Effectuer une recherche"
-          className="w-full h-10 bg-gray-800 text-white border border-gray-700 rounded-md pl-3 pr-3 py-2 resize-none overflow-hidden focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="w-full h-10 bg-gray-900 text-white border border-gray-700 rounded-md pl-3 pr-3 py-2 resize-none overflow-hidden focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
       </div>
     </div>
--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -4,10 +4,9 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="h-screen w-64 fixed top-0 left-0 bg-gray-800 text-white flex flex-col border-r-1 border-gray-500 p-4 z-50">
-      <h1 className="text-2xl font-bold mb-4">Mon App</h1>
-      <hr className="w-full border-t-1 border-gray-500 mb-6" />
-      <nav className="flex flex-col gap-4">
+    <div className="h-screen w-64 fixed top-0 left-0 bg-[#212631] text-white flex flex-col border-r-1 border-gray-500 z-50">
+      <h1 className="text-2xl font-bold mb-4 p-4">Mon App</h1>
+      <nav className="flex flex-col gap-4 p-4">
         <Link href="/" className="hover:text-gray-300">Accueil</Link>
         <Link href="/Dashboard/Crypto" className="hover:text-gray-300">Crypto</Link>
         <Link href="/Dashboard/Profile">Profile</Link>
