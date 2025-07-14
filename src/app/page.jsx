a2a81ae19e8f515fa06c15c9423221fe84d33f6d
TISEPSE <titi21player@gmail.com>
2025-07-14 16:54:50 +0200

    daisyui fonctionelle

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold">Page d'accueil</h1>
       <p className="mt-4 text-lg">Bienvenue sur notre application Next.js !</p>
+      <a className="btn btn-primary">Button</a>
     </div>
   );
 }
